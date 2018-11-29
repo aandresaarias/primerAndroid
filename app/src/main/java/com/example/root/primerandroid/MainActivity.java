457574a5ac8424694b1a1065fb1f5a5ea36a3807
afas <aandresaarias@hotmail.com>
2018-11-29 11:30:39 -0500

    prueba clone

--- app/src/main/java/com/example/root/primerandroid/MainActivity.java
+++ app/src/main/java/com/example/root/primerandroid/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.root.primerandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        //agregando comentarios con fines de pruebas
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
