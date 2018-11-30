2f3d3d62bcae4b57a4a2160f8313f67cfdbe45c2
afas <aandresaarias@hotmail.com>
2018-11-30 17:04:18 -0500

    creando botones y textview

--- app/src/main/java/com/example/root/primerandroid/MainActivity.java
+++ app/src/main/java/com/example/root/primerandroid/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.root.primerandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    TextView textView;
+    Button boton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        //agregando comentarios con fines de pruebas
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textUno);
+        boton = (Button) findViewById(R.id.boton);
+
+        boton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+               textView.setText("nuevo texto");
+            }
+        });
     }
 }
