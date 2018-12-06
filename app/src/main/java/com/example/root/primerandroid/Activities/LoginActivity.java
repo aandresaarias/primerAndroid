09c48ee61603dabae9d257607f407124f78e75d5
afas <aandresaarias@hotmail.com>
2018-12-06 17:48:58 -0500

    validaciones de regristro  con sharedpreferences creacion actividad registrar

--- app/src/main/java/com/example/root/primerandroid/Activities/LoginActivity.java
+++ app/src/main/java/com/example/root/primerandroid/Activities/LoginActivity.java
@@ -1,5 +1,8 @@
 package com.example.root.primerandroid.Activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +25,12 @@ public class LoginActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        SharedPreferences sharedPreferences = getSharedPreferences("usuarios", Context.MODE_PRIVATE);
+        SharedPreferences.Editor sharedEditor = sharedPreferences.edit();
+        sharedEditor.putString("usuario", "afas");
+        sharedEditor.putString("pass", "afas");
+        sharedEditor.commit();
+
         loginBoton = (Button) findViewById(R.id.loginBoton);
         loginRegistro = (Button) findViewById(R.id.loginRegistro);
         etNombreLogin = (EditText) findViewById(R.id.etNombreLogin);
@@ -32,7 +41,30 @@ public class LoginActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                Toast.makeText(LoginActivity.this, "dIO click EN LOGIN", Toast.LENGTH_LONG).show();
+                //Toast.makeText(LoginActivity.this, "dIO click EN LOGIN", Toast.LENGTH_LONG).show();
+                String usuario = etNombreLogin.getText().toString();
+                String contrasena = etPassLogin.getText().toString();
+
+                SharedPreferences sharedLogin = getSharedPreferences("usuarios", Context.MODE_PRIVATE);
+
+                if (usuario.equals(sharedLogin.getString("usuario","x")))
+                {
+                    if (contrasena.equals(sharedLogin.getString("pass","y")))
+                    {
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                        else
+                            {
+                                Toast.makeText(LoginActivity.this,"contraseña invalida",Toast.LENGTH_SHORT).show();
+                            }
+                }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this,"usuario invalido",Toast.LENGTH_LONG).show();
+                    }
+
             }
         });
 
@@ -41,7 +73,8 @@ public class LoginActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                   Toast.makeText(LoginActivity.this,"click en registro",Toast.LENGTH_SHORT).show();
+                   Intent intent = new Intent(LoginActivity.this, RegistrarActivity.class);
+                   startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/root/primerandroid/Activities/RegistrarActivity.java
@@ -0,0 +1,17 @@
+package com.example.root.primerandroid.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.root.primerandroid.R;
+
+public class RegistrarActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+}
