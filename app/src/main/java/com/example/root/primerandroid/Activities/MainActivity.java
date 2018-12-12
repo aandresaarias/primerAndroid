3d2d69ecf9aebdd8125c9092fdd3d811fea6ccd7
afas <aandresaarias@hotmail.com>
2018-12-12 17:17:59 -0500

    matterial dessing, drawer layout, navigation web, menu

--- app/src/main/java/com/example/root/primerandroid/Activities/MainActivity.java
+++ app/src/main/java/com/example/root/primerandroid/Activities/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.root.primerandroid.Activities;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 import com.example.root.primerandroid.R;
 
 public class MainActivity extends AppCompatActivity
 {
-    TextView textView;
-    Button boton;
-
+    Toolbar toolbar;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,17 +26,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textUno);
-        boton = (Button) findViewById(R.id.boton);
+        toolbar = (Toolbar) findViewById(R.id.toolBarMainActivity);
+        setSupportActionBar(toolbar);
 
-
-        boton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-               textView.setText("nuevo texto");
-            }
-        });
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerActivityMain);
+        navigationView = (NavigationView) findViewById(R.id.navigationViewActivityMain);
     }
 }
