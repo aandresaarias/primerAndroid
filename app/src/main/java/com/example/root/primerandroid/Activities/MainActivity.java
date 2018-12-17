b96dbad9a96c370a3fd2802ee1c7833a9b8bb16f
afas <aandresaarias@hotmail.com>
2018-12-17 12:08:29 -0500

    uso de drawerLayout,  configuracion de menu y animaciones

--- app/src/main/java/com/example/root/primerandroid/Activities/MainActivity.java
+++ app/src/main/java/com/example/root/primerandroid/Activities/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.root.primerandroid.Activities;
 
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -31,5 +34,41 @@ public class MainActivity extends AppCompatActivity
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerActivityMain);
         navigationView = (NavigationView) findViewById(R.id.navigationViewActivityMain);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                drawerLayout.closeDrawers();
+
+                return false;
+            }
+        });
+
+        android.support.v7.app.ActionBarDrawerToggle actionBarDrawerToggle =
+                new android.support.v7.app.ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        {
+            @Override
+            public void onDrawerOpened(View drawerView)
+            {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView)
+            {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        actionBarDrawerToggle.syncState();
+
+
     }
 }
